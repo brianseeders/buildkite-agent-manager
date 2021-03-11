15950121a2e181b6473ae58187250c0aff99f0a7
Brian Seeders <brian.seeders@elastic.co>
2021-03-11 14:30:58 -0500

    Switch zone

--- src/defaultAgentConfig.ts
+++ src/defaultAgentConfig.ts
@@ -3,8 +3,8 @@ import { TopLevelConfig } from './agentConfig';
 const defaultConfig: TopLevelConfig = {
   gcp: {
     project: 'elastic-kibana-184716',
-    // zone: 'us-central1-b',
-    zone: 'us-west1-a',
+    zone: 'us-central1-b',
+    // zone: 'us-west1-a',
     serviceAccount: 'elastic-buildkite-agent@elastic-kibana-184716.iam.gserviceaccount.com',
     agents: [
       {
